85409ac3d69ac905e6ff115b53be5e5022f6dc3c
William MacDonald <will@cyberline.com>
2023-03-29 18:39:30 -0600

    starting on modules

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
+
+@Controller('cats')
+export class CatsController {
+    constructor(private catsService: CatsService) {}
+
+    @Post()
+    async create(@Body() createCatDto: CreateCatDto) {
+        this.catsService.create(createCatDto);
+    }
+
+    @Get()
+    async findAll(): Promise<Cat[]> {
+        return this.catsService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string): string {
+        return `This action returns a #${id} cat`;
+    }
+}
