74ec3b385bdc551f7c812e0d69ced8d1c28ae102
William MacDonald <will@cyberline.com>
2023-03-31 13:14:19 -0600

    cats - finished overview of docs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { CatsController } from './cats/cats.controller';
 import { CatsModule } from './cats/cats.module';
 import { logger } from './common/middleware/logger.middleware';
-import { ChatModule } from './chat/chat.module';
 
 @Module({
-    imports: [CatsModule, ChatModule],
+    imports: [CatsModule],
 })
 export class AppModule implements NestModule {
     configure(consumer: MiddlewareConsumer) {
